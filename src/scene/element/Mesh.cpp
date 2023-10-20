7fb9adef5ca5a401603e39a779af76d4b04adea6
Goyacyt <1412974228@qq.com>
2023-10-20 21:11:07 +0800

    construct TIN

--- src/scene/element/Mesh.cpp
+++ src/scene/element/Mesh.cpp
@@ -1,9 +1,111 @@
-#include"Mesh.h"
+#include "Meshdata.h"
 
-MeshData::MeshData() {
-    //TODO
-    return;
+MeshData::MeshData(char* path) {
+	MeshPath = path;
+	GDALAllRegister();
+	GDALDataset* poDataset;
+
+	poDataset = (GDALDataset*)GDALOpen(path, GA_ReadOnly);
+	if (poDataset == NULL) {
+		cout << "The file cannot be opened" << endl;
+		return;
+	}
+
+	ImgSizeX = poDataset->GetRasterXSize();
+	ImgSizeY = poDataset->GetRasterYSize();
 }
+
+int MeshData::GetImgSizeX() {
+	return ImgSizeX;
+}
+int MeshData::GetImgSizeY() {
+	return ImgSizeY;
+}
+
+GDALDataset* createRaster(char* filename, int nRow, int nCol, GDALDataType type)
+{
+	GDALDriver* driver = GetGDALDriverManager()->GetDriverByName("GTiff");
+	GDALDataset* ds = driver->Create(
+		filename,
+		nRow,
+		nCol,
+		1,
+		type,
+		NULL);
+	return ds;
+}
+
+TIN MeshData::GetMesh(int x1, int y1, int x2, int y2) {
+	GDALAllRegister();
+	GDALDataset* poDataset;
+
+	CGAL::Point_set_3<Point_3> points;
+	TIN dsm0(points.points().begin(), points.points().end());
+
+	poDataset = (GDALDataset*)GDALOpen(MeshPath, GA_ReadOnly);
+	if (poDataset == NULL) {
+		cout << "The file cannot be opened" << endl;
+		return dsm0;
+	}
+
+	int nImgSizeX = ImgSizeX;
+	int nImgSizeY = ImgSizeY;
+	//cout << nImgSizeX << "  *  " << nImgSizeY << endl;
+
+	//获取坐标变换系数 
+	double trans[6];
+	poDataset->GetGeoTransform(trans);
+	double dx = trans[1];
+	double startx = trans[0] + 0.5 * dx;
+	double dy = -trans[5];
+	double starty = trans[3] - nImgSizeY * dy + 0.5 * dy;
+
+	//获取图像波段 
+	GDALRasterBand* poBand;
+	int bandcount;
+	bandcount = poDataset->GetRasterCount();
+
+	//读数据
+	int* pafScanline;
+	pafScanline = new int[nImgSizeX * nImgSizeY];
+	poBand = poDataset->GetRasterBand(bandcount);
+	poBand->RasterIO(GF_Read, 0, 0, nImgSizeX, nImgSizeY, pafScanline, nImgSizeX, nImgSizeY, GDALDataType(poBand->GetRasterDataType()), 0, 0);
+
+
+	int num_image_size = 0;
+
+	if (x1 < 0 || x1 >= nImgSizeX || y1 < 0 || y1 >= nImgSizeY) {
+		cout << "coordinate out of range" << endl;
+		return dsm0;
+	}
+	if (x2 < 0 || x2 >= nImgSizeX || y2 < 0 || y2 >= nImgSizeY) {
+		cout << "coordinate out of range" << endl;
+		return dsm0;
+	}
+
+	for (int i = x1; i <= x2; i++) {
+		for (int j = y1; j <= y2; j++) {
+			num_image_size++;
+			cout << pafScanline[i * nImgSizeY + j] << " ";
+			cout << num_image_size << endl;
+
+			double gx = startx + dx * i;
+			double gy = starty + dy * j;
+			size_t m = (size_t)i * nImgSizeY + j;
+			//tinyCG::Vec3d P(gx, gy, pafScanline[m]);
+			points.insert(Point_3(gx, gy, pafScanline[m]));
+		}
+	}
+
+	//生成三角网格
+	TIN dsm(points.points().begin(), points.points().end());
+	//输出三角网格的顶点数目
+	cout << dsm.number_of_vertices() << endl;
+
+	return dsm;
+}
+
+
 /*
 MeshData::MeshData(std::vector<Triangle>triangles) {
 	//TODO
@@ -24,3 +126,4 @@ std::shared_ptr<std::vector<Triangle>> MeshData::getMesh(){
 	return ptr;
 }
 */
+
--- src/scene/element/Mesh.h
+++ src/scene/element/Mesh.h
@@ -1,19 +1,56 @@
 #pragma once
 
-#include"Element.h"
-#include"Triangle.h"
+#include <iostream>
 #include<vector>
 
-class MeshData : public Element {
-    //std::vector<Triangle>triangles;
-public:
-    MeshData();
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/convex_hull_2.h>
+#include <gdal.h>
+#include <gdal_priv.h>
 
-    //MeshData(std::vector<Triangle>triangles);
-    //bool insertTriangle(Triangle t);
-    //bool insertTriangle(const Eigen::Vector3f& point1, const Eigen::Vector3f& point2, const Eigen::Vector3f& point3);
-    virtual std::shared_ptr<std::vector<Triangle>> getMesh() override {
-        Triangle t;
-        return t.getMesh();
-    };
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Projection_traits_xy_3.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/Triangulation_vertex_base_with_info_2.h>
+#include <CGAL/Triangulation_face_base_with_info_2.h>
+#include <CGAL/boost/graph/graph_traits_Delaunay_triangulation_2.h>
+#include <CGAL/boost/graph/copy_face_graph.h>
+#include <CGAL/Point_set_3.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/Polygon_mesh_processing/border.h>
+#include <CGAL/Polygon_mesh_processing/remesh.h>
+
+using Kernel = CGAL::Exact_predicates_inexact_constructions_kernel;
+using Projection_traits = CGAL::Projection_traits_xy_3<Kernel>;
+using TIN = CGAL::Delaunay_triangulation_2<Projection_traits>;
+
+using Point_3 = Kernel::Point_3;
+typedef int BYTE;
+
+class MeshData : public Element {
+	//std::vector<Triangle>triangles;
+public:
+	MeshData(char* path);
+	int GetImgSizeX();
+	int GetImgSizeY();
+	TIN GetMesh(int x1, int y1, int x2, int y2);
+
+	//TODO()
+
+	//MeshData(std::vector<Triangle>triangles);
+	//bool insertTriangle(Triangle t);
+	//bool insertTriangle(const Eigen::Vector3f& point1, const Eigen::Vector3f& point2, const Eigen::Vector3f& point3);
+private:
+	char* MeshPath;
+	int ImgSizeX;
+	int ImgSizeY;
+
+	//MeshData(std::vector<Triangle>triangles);
+	//bool insertTriangle(Triangle t);
+	//bool insertTriangle(const Eigen::Vector3f& point1, const Eigen::Vector3f& point2, const Eigen::Vector3f& point3);
+	/*virtual std::shared_ptr<std::vector<Triangle>> getMesh() override {
+		Triangle t;
+		return t.getMesh();
+	 };
+	 */
 };
